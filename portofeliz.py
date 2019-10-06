46e8c3b3b0af852e9f85e2429ea05082f8c1f1bb
felipedamin <felipes.damin96@gmail.com>
2019-10-06 01:04:07 -0300

    trabalhando arquivo de obras

--- portofeliz.py
+++ portofeliz.py
@@ -3,11 +3,37 @@ import pandas as pd
 ## db = pd.read_csv('./portofeliz/despesas-porto-feliz-2018(2).csv', sep=';', encoding='latin-1')
 
 db = pd.read_csv('iegm.csv', sep=';', encoding='latin-1', low_memory=False)
+db = pd.read_csv('obras-paralisadas.csv', sep=';', encoding='UTF-8', low_memory=False)
+
+a = [
+'Esgotamento sanitário - Rede coletora, estações de tratamento e similares',
+'Abastecimento de água - Captação, Adução, Tratamento e similares',
+'Saneamento',
+'Transporte - Ferrovias',
+'Energia - distribuição (rede interna urbana e similares)',
+'Transporte - Hidrovias',
+'Recursos Hídricos - Canais',
+'Infraestrutura Turística',
+'Energia - transmissão (subestações e linhas de transmissão)',
+'Macro drenagem - Canais e Rios',
+'Transporte - Estações Ferroviárias'
+'Energia - combustíveis e derivados',
+'Rede de Drenagem de águas pluviais'
+]
+
+
+print(db.shape)
+drop = []
+for i in range(len(db['classificacao'])):
+    if db['classificacao'][i] not in a:
+        drop.append(i) 
+print(len(drop))
+db = db.drop(drop)
 
 for i in db.head():
     print(i, end=', ')
 print('\n')
-
+print(db.shape)
 
 perguntas = {}
 '''
@@ -30,6 +56,7 @@ for i in range(len(db['texto_pergunta_genérica'])):
 print(perguntas.keys())
 '''
 
+'''
 lista = []
 for i in range(len(db['municipio'])):
     if db['municipio'][i] == 'Porto Feliz':
@@ -38,7 +65,7 @@ for i in range(len(db['municipio'])):
 
 for i in lista:
     print(i)    
-
+'''
 ## print(perguntas)
 ## print(perguntas)
 ## ['ano_exercicio', 'ds_municipio', 'ds_orgao', 'mes_referencia', 'tp_despesa', 'vl_despesa', 'ds_funcao_governo', 'ds_subfuncao_governo', 'cd_programa', 'cd_acao', 'ds_fonte_recurso', 'ds_cd_aplicacao_fixo', 'ds_modalidade_lic']
