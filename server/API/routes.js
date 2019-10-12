1486705da3b50241f1bd50e4a114e475e3705a15
felipedamin <felipes.damin96@gmail.com>
2019-10-12 13:35:48 -0300

    working on routes

--- server/API/routes.js
+++ server/API/routes.js
@@ -1,12 +1,58 @@
 const express = require('express');
+const getData = require('../getData/index');
+const Cidade = require('../database/models/cidade')
 
 const router = express.Router();
 
-router.get('/caminhoDaRequisiçao', async (req, res) => {
-    // procura no banco de dados
-    // Se achar, devolve
+// rota para buscar nomes dos municipios
+router.get('/municipios', async (req, res) => {
+    try {
+        // procura no banco de dados
+        const cidade = await Cidade.findOne({ municipio: 'adamantina' });
 
-    // Se nao achar, chama o getData, depois salva no BD e devolve (?)
+        // Se nao achar, chama o getData,
+        if (cidade == '') {
+            await getData.getMunicipios();
+            var municipios = require('../database/municipios.json')
+
+            for (mun in municipios) {
+                await Cidade.create({ municipio: mun.municipio, municipio_extenso: mun.municipio_extenso })
+            }
+            return res.status(200).send('atualizado')
+            return res.status(200).send(`<p>${JSON.stringify(municipios)}</p>`)
+        }
+        // depois salva no BD e devolve (?)
+        return res.send({ cidade });
+    } catch (err) {
+        return res.status(400).send(err)
+    }
 });
+
+
+// rota para buscar IEGM do municipio
+router.get('/iegm/:ano/:municipio', async (req, res) => {
+    try {
+        const cidade = await Cidade.findOne({ ...req.params.municipio });
+
+        //findOne().populate('iegm')
+        /*
+        const iegm = await "iegm".concat(req.params.ano)
+        // query database
+        const db = await client.db('deephack2019');
+        return res.send(test);
+        const result = await db.find({ 'a': req.params.municipio });
+        // return iegm
+        */
+        return res.send(cidade);
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+});
+
+// rota para buscar despesas do municipio
+
+// rota para buscar saneamento do municipio
+
+// rota para buscar residuos do municipio
 
 module.exports = app => app.use('/database', router) // '/database' é o caminho para esse router
